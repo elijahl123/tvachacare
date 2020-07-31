5084fffd11985fdae810283ea3780d361b25f2a0
Elijah <elijah.kane.1972@gmail.com>
2020-07-31 00:16:37 -0700

    Lots of Stuff

--- patientInformation/admin.py
+++ patientInformation/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import PatientInformation
+
+from .models import PatientInformation, Account
 
 # Register your models here.
 admin.site.register(PatientInformation)
+admin.site.register(Account)
