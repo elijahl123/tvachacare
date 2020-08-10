351581e91a3cfa24f8945a0f49d56757ba6e56ee
Elijah <elijah.kane.1972@gmail.com>
2020-08-10 01:56:59 -0700

    Conf

--- TvachaCare/wsgi.py
+++ TvachaCare/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TvachaCare.settings')
 
 application = get_wsgi_application()
+
+sys.path.append('/opt/bitnami/apps/django/django_projects/tvachacare')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/tvachacare/egg_cache")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TvachaCare.settings')
